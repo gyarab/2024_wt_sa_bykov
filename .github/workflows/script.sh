45fdb603fb6818d6f65b0c4b34f6b4f757999974
Martin <84806126+MegapolisPlayer@users.noreply.github.com>
2024-09-23 18:35:43 +0200

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,4 +1,4 @@
 # Script for file copying
 
-cp /webtech/13sep-hw/LINUSTORVALDS.md /referat.md
-cp /webtech/20sep-hw/COLOR.md /color.md
+cp webtech/13sep-hw/LINUSTORVALDS.md referat.md
+cp webtech/20sep-hw/COLOR.md color.md
