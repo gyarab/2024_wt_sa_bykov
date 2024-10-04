f87fda160f62a725b6fe018cfdf80a0a2b41ac51
Martin Bykov <martin.dolgov.s@gyarab.cz>
2024-10-04 10:02:22 +0200

    added finding of largest shape in oct4-cl

--- programko/oct4-cl/Main.java
+++ programko/oct4-cl/Main.java
@@ -1,11 +1,26 @@
+import java.util.*;
 
 public class Main {
+    public static Shape getLargest(List<Shape> parameterListOfShapes) {
+        Shape largestShape = parameterListOfShapes.get(0);
+        for(Shape shape : parameterListOfShapes) {
+            if(shape.countArea() > largestShape.countArea()) {
+                largestShape = shape;
+            }
+            //System.out.println(shape); //we override ToString for simplicity
+        }
+        return largestShape;
+    }
     public static void main(String[] args) throws Exception {
-        Shape s = new Circle(5);
-        System.out.println(s); //we override ToString for simplicity
-        s = new Rectangle(5, 10);
-        System.out.println(s);
-        s = new Triangle(6, 6, 11);
-        System.out.println(s);
+        ArrayList<Shape> list = new ArrayList<Shape>();
+        list.add(new Rectangle(5, 10));
+        list.add(new Triangle(6, 6, 11));
+        list.add(new Circle(5));
+        
+        System.out.println("Largest - "+getLargest(list));
+
+        for(Shape shape : list) {
+            System.out.println("Other - "+shape);
+        }
     }
 }
